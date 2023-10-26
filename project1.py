563c691999fdd93c620acae798098969ed47d8ed
ist1100120 <alexandre.f.coelho@tecnico.ulisboa.pt>
2023-10-26 01:01:54 +0100

    cleanup

--- project1.py
+++ project1.py
@@ -4,6 +4,7 @@
 
 import sys
 
+from inout import parse_input, generate_output
 from pysat.examples.rc2 import RC2
 from pysat.formula import WCNF
 from pysat.pb import PBEnc
@@ -11,66 +12,11 @@ from pysat.pb import PBEnc
 from networkx import DiGraph, topological_sort
 
 
-def parse_input(input_str: str):
-    """Parses the input file. The input file provided is assumed to be correct
-
-    Args:
-        input_str (str): Input to parse
-
-    Returns:
-        n (int): Number of groups of rules
-        m (int): Number of switches
-        mr (list[int]): List of memory requirement
-        ns (list[int]): List of number of stages
-        mc (list[int]): List of memory capacity
-        d (int): Number of dependencies
-        f (list[(list[int])]): List of rules for forwarding
-    """
-    if input_str[-1] != '\n':
-        input_str += '\n'
-    lines = input_str.split('\n')[:-1]
-    n, m = [int(line) for line in lines[:2]]
-    mr = [int(i) for i in lines[2].split(' ')]
-    ns = [int(i) for i in lines[3].split(' ')]
-    mc = [int(i) for i in lines[4].split(' ')]
-    d = int(lines[5])
-    f = [[int(i) for i in line.split(' ')] for line in lines[6: (6 + d)]]
-
-    return n, m, mr, ns, mc, d, f
-
-
-def generate_output(recirculations: int, order: list, placements: list):
-    """Generates the string to be output to solution.txt
-
-    Args:
-        recirculations (int): Overall number of re-circulations
-        order (list[int]): Order of the switches
-        placements (list[(list[list[int]])]): Placement of groups of rules in each stage of each switch
-
-    Returns:
-        out (str): String to be printed to solution.txt
-    """
-    out = f"{recirculations}\n"
-
-    for i in range(len(order)):
-        out += f"{order[i]} "
-
-    out = out[:-1]
-    out += "\n"
-
-    for switch in placements:
-        for stage in switch:
-            for rule in stage:
-                out += f"{rule} "
-            out = out[:-1]
-            out += ", "
-        out = out[:-2]
-        out += "\n"
-
-    return out
-
-
-def create_coding(n: int, m: int, ns: list):  # -> tuple[Callable[..., int], Callable[..., tuple[int, int, int]]]:
+def create_coding(
+        n: int,
+        m: int,
+        ns: list
+):
     """Creates the encoding and decoding functions which convert between problem variables and SAT
 
     Args:
